07a4aacdd4f1d62942fe76c42332591ef68ace65
Marshall.Tuinier <Mtuinier@gmail.com>
2019-01-06 16:29:16 -0800

    test for bg

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -48,7 +48,7 @@ class Layout extends Component {
                 left: 0,
                 top: 0,
                 width: "100%",
-                height: "120%",
+                height: "100vh",
               }}
             />
             <BgMask />
