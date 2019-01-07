da8499863afe6da105bf6c87423f35c7a35acbb2
Marshall.Tuinier <Mtuinier@gmail.com>
2019-01-06 16:33:25 -0800

    fixed background jitter

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -48,7 +48,7 @@ class Layout extends Component {
                 left: 0,
                 top: 0,
                 width: "100%",
-                height: "100vh",
+                height: "110vh",
               }}
             />
             <BgMask />
