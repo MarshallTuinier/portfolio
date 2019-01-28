4f1e10bb9f4fa4832d9c903a7cdecc118fea11dd
Marshall Tuinier <mtuinier@gmail.com>
2019-01-28 11:26:44 -0800

    fix wrong URL

--- src/pages/Stockwatch.js
+++ src/pages/Stockwatch.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { graphql } from "gatsby"
-import Img from "gatsby-image"
-import styled from "styled-components"
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
+import styled from "styled-components";
 import Layout from "../components/Layout";
 import Subpage from "../components/Subpage";
-import Github from '../assets/svg/github'
+import Github from "../assets/svg/github";
 export default class About extends Component {
   render() {
     return (
@@ -12,16 +12,36 @@ export default class About extends Component {
         <Subpage>
           <h1>StockWatch</h1>
           <p>
-            StockWatch is a frontend web application built using React and D3. I wanted to devote some time to learn the ever-intimidating D3 charting library and its implementation into React, and this is the result. 
+            StockWatch is a frontend web application built using React and D3. I
+            wanted to devote some time to learn the ever-intimidating D3
+            charting library and its implementation into React, and this is the
+            result.
           </p>
           <a href="https://stock-watch.marshalltuinier.com">
-          <Img fluid={this.props.data.stockwatchFull.childImageSharp.fluid} style={{ maxWidth: "1000px", margin: "2rem auto", borderRadius: "5px"}} />
+            <Img
+              fluid={this.props.data.stockwatchFull.childImageSharp.fluid}
+              style={{
+                maxWidth: "1000px",
+                margin: "2rem auto",
+                borderRadius: "5px"
+              }}
+            />
           </a>
-          <p>View the full application <a href="https://stockwatch.marshalltuinier.com">HERE</a>.</p>
-          
-          <p>Code: {'  '} <StyledLink href="https://github.com/marshalltuinier/stock-watch" target="_blank" rel="noopener noreferrer">
+          <p>
+            View the full application{" "}
+            <a href="https://stock-watch.marshalltuinier.com">HERE</a>.
+          </p>
+
+          <p>
+            Code: {"  "}{" "}
+            <StyledLink
+              href="https://github.com/marshalltuinier/stock-watch"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <Github />
-            </StyledLink></p>
+            </StyledLink>
+          </p>
         </Subpage>
       </Layout>
     );
@@ -38,12 +58,12 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 const StyledLink = styled.a`
   svg {
-    transition: transform .25s ease;
+    transition: transform 0.25s ease;
     &:hover {
       transform: scale(1.2);
     }
   }
-`
+`;
